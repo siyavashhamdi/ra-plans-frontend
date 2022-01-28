fc07be7f61c441d6f9b3c336e3dd56945301af1f
Siyavash Hamdi <siyavashhamdi@gmail.com>
2022-01-28 16:29:42 +0330

    Init project and simplified the source

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello React App!
+    </div>
+  );
+}
+
+export default App;
